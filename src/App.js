087b143b38d9a88e70f6e0ecc4b2453d424e63c5
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-03 11:14:28 -0500

    add my sub header with city temp and switch mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Cdtsm from './components/Cdtsm'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Cdtsm />
+      <NavBar />
+
 
     </div>
   );
---
+++ src/components/Cdtsm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Cdtsm.css'
+
+const cdtsm = () => {
+  return <section className="cdtsm-style">
+    <section className="ctd-style">
+        <div className="city-name">
+            keller |
+        </div>
+        <div className="date">
+            Thurs-3 |
+        </div>
+        <div className="temp">
+            107°F
+        </div>
+    </section>
+    <section className="s-mode">
+        <img src="" alt="logo" className="logo" />
+        <h5>Switch Mode</h5>
+    </section>
+  </section>
+}
+
+export default cdtsm
