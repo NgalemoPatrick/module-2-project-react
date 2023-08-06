14e552740a0391e8d44aec8fe3427bb454131dda
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-06 01:05:36 -0500

    add keyframe animation

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,55 +5,55 @@ const projects = () => {
   return (
     <section className="container">
       {/* project 1 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 1" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 2 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 2" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 3 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 3" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 4 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 4" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 5 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Projct 5" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 6" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 5 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 7" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 6 section */}
-      <div className="project-1">
+      <div className="project-1 bounce">
         <div className="project-1-image">
           <img src="" alt="Project 8" className="procj-1" />
         </div>
