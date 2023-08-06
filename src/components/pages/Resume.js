b1c6a292cdde1a8265f3808809cf07c9dffff57b
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-05 23:31:08 -0500

    add my resume in public folder and create a function to handle the downloading

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const Resume = () => {
+    // function to download my resume
+    const handleDownload = () => {
+        // let fetch the resume from the public folder
+        fetch('resume.pdf').then(res => {
+            res.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                // setting propertu value
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'resume.pdf';
+                alink.click();
+            })
+        })
+    }
   return (
     <div className='resume-style'>
         
         <div className="resu-style">
             Resume
         </div>
-        <div className="download">
+        <div className="download" onClick={handleDownload}>
             DOWNLOAD
         </div>
     </div>
