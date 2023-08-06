e66b6e9a8ea3365bb8f75c1930ded7946d6beca4
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-06 00:25:56 -0500

    wire-frame my projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import '../Project.css'
 
 const projects = () => {
   return (
-    <div>projects</div>
-  )
-}
+    <section className="container">
+      {/* project 1 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      {/* project 2 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      {/* project 3 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      {/* project 4 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      {/* project 5 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      {/* project 5 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+      {/* project 6 section */}
+      <div className="project-1">
+        <div className="project-1-image">
+          <img src="" alt="" className="procj-1" />
+        </div>
+        <div className="procject-1-description"></div>
+      </div>
+    </section>
+  );
+};
 
-export default projects
+export default projects;
