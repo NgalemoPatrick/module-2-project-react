c339a9ddc58881fb602ffe8a3fd39c255dd6204e
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-06 00:41:16 -0500

    add some style to project.css

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -7,55 +7,55 @@ const projects = () => {
       {/* project 1 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 1" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 2 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 2" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 3 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 3" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 4 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 4" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 5 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Projct 5" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 6" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 5 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 7" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
       {/* project 6 section */}
       <div className="project-1">
         <div className="project-1-image">
-          <img src="" alt="" className="procj-1" />
+          <img src="" alt="Project 8" className="procj-1" />
         </div>
         <div className="procject-1-description"></div>
       </div>
