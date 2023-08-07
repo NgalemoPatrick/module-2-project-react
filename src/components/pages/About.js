96c113680d454187d7233e2d10e028ef104aefdc
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-06 21:32:56 -0500

    add icons

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,26 @@
 import React from "react";
-import '../About.css'
+import "../About.css";
 import { FaReact, FaJs, FaNodeJs, FaMongoDb, FaPython } from "react-icons/fa";
 const about = () => {
-  return <section className="profile-img">
-    <div className="photo">
+  return (
+    <section className="profile-img">
+      <div className="photo">
         <img src="" alt="My picture" className="my-pic" />
-    </div>
-    <p>Software Engineer providing freelancing services 
-        for programming and designing contents. Navigate the <span>PROJECTS Link </span> 
-        to see some of my work. For more information about me please 
-        feel free to contact me.
-    </p>
-    <div className="icons-style">
-
-    </div>
-  </section>;
+      </div>
+      <p>
+        Software Engineer providing freelancing services for programming and
+        designing contents. Navigate the <span>PROJECTS Link </span>
+        to see some of my work. For more information about me please feel free
+        to contact me.
+      </p>
+      <div className="icons-style">
+        <FaJs size="1.5rem" color="#5d845f" />
+        <FaReact size='1.5rem' color='#5d845f'/>
+        <FaNodeJs size='1.5rem' color='#5d845f' />
+        <FaPython size='1.5rem' color='#5d845f' />
+      </div>
+    </section>
+  );
 };
 
 export default about;
