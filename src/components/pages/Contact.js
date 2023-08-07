8d2786dfb1459c018b99154578996e031c6ce2b2
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-07 09:31:55 -0500

    read me

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -19,7 +19,6 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
-        
         },
         (error) => {
           console.log(error.text);
@@ -30,7 +29,7 @@ const Contact = () => {
     <div className="form-style">
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="from_name" />
         <label>Email</label>
         <input type="email" name="user_email" />
         <label>Message</label>
